4a7f04fcc141c51cef239f03b8d14ac705fc67d7
Leonidkuchmiy <60760010+Leonidkuchmiy@users.noreply.github.com>
2021-03-14 23:58:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,14 @@
-<?php
+$body = file_get_contents('php://input'); 
+$arr = json_decode($body, true); 
+ 
+include_once ('telegramgclass.php');   
 
-# Принимаем запрос
-$data = json_decode(file_get_contents('php://input'), TRUE);
+$tg = new tg('токен который вы получили');
 
-//https://api.telegram.org/bot*TOKEN*/setwebhook?url=*URL*
+$chat_id = $arr['message']['chat']['id'];
+$userTgId = $arr['message']['from']['id'];
+$text = $arr['message']['text'];
+$coord1 = $arr['message']['location']['latitude'];
+$coord2 = $arr['message']['location']['longitude'];
 
-# Переменные
-$token = '1681935656:AAGG7bpYJFly7eMWpGnondSOrguLtT27mLo';
-
-# Обрабатываем команды
-$message = $data['message']['text'];
-
-# Формируем массив для отправления в телеграм
-$params = [
-    'chat_id' => $data['message']['chat']['id'],
-    'text'    => $message
-];
-
-# Отправляем запрос в телеграм
-file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($params));
-?>
+$tg->send($chat_id, "Нас не догонят!");
