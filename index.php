002c3bf509b2a87a9c487c3493a5e0119225e340
Leonidkuchmiy <60760010+Leonidkuchmiy@users.noreply.github.com>
2021-03-14 23:50:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
+<?php
+
+# Принимаем запрос
 $data = json_decode(file_get_contents('php://input'), TRUE);
 file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
-//('1681935656:AAGG7bpYJFly7eMWpGnondSOrguLtT27mLo'); //Устанавливаем токен, полученный у BotFather
-   
+
+//https://api.telegram.org/bot*TOKEN*/setwebhook?url=*URL*
+
+# Переменные
+$token = '';
+
+# Обрабатываем команды
+$message = $data['message']['text'];
+
+# Формируем массив для отправления в телеграм
+$params = [
+    'chat_id' => $data['message']['chat']['id'],
+    'text'    => $message
+];
+
+# Отправляем запрос в телеграм
+file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($params));
