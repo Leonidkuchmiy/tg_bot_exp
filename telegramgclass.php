b41bec64fa0d664cff0e062b7e155f96bd4b4eaa
Leonidkuchmiy <60760010+Leonidkuchmiy@users.noreply.github.com>
2021-03-15 23:20:00 +0300

    Update telegramgclass.php

--- telegramgclass.php
+++ telegramgclass.php
@@ -1,3 +1,4 @@
+<?php
 class TG {
   
     public $token = '1681935656:AAGG7bpYJFly7eMWpGnondSOrguLtT27mLo';
@@ -6,63 +7,69 @@ class TG {
         $this->token = $token; 
     }
       
-    public function send($id, $message,$keyboard) {   
-		
-		//Удаление клавы
-		if($keyboard == "DEL"){		
-			$keyboard = array(
-				'remove_keyboard' => true
-			);
-		}
-		if($keyboard){
-			//Отправка клавиатуры
-			$encodedMarkup = json_encode($keyboard);
-			
-			$data = array(
-				'chat_id'      => $id,
-				'text'     => $message,
-				'reply_markup' => $encodedMarkup
-			);
-		}else{
-			//Отправка сообщения
-			$data = array(
-				'chat_id'      => $id,
-				'text'     => $message
-			);
-		}
-       
-        $out = $this->request('sendMessage', $data);       
-        return $out;
-    }         
-	
-	public function getPhoto($data){
-		$out = $this->request('getFile', $data);        
-        return $out;
-	}  
-	
-	public function savePhoto($url,$puth){
-		$ch = curl_init('https://api.telegram.org/file/bot' . $this->token .  '/' . $url);
-		$fp = fopen($puth, 'wb');
-		curl_setopt($ch, CURLOPT_FILE, $fp);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_exec($ch);
-		curl_close($ch);
-		fclose($fp);
-	}
-	
+    public function send($id, $message, $kb) {
+        $data = array(
+            'chat_id' => $id,
+            'text'  => $message,
+            'parse_mode' => 'HTML',
+            'disable_web_page_preview'=>true,
+            'reply_markup' => json_encode(array('inline_keyboard' => $kb))
+        );
+        $this->request('sendMessage', $data);
+    }  
+
+    public function editMessageText($id, $m_id, $m_text, $kb=''){
+        $data=array(
+             'chat_id' => $id,
+             'message_id' => $m_id,
+             'parse_mode' => 'HTML',
+             'text' => $m_text
+        );
+        if($kb)
+            $data['reply_markup']=json_encode(array('inline_keyboard' => $kb));
+
+        $this->request('editMessageText', $data); 
+    }
+
+
+    public function editMessageReplyMarkup($id, $m_id, $kb){
+        $data=array(
+             'chat_id' => $id,
+             'message_id' => $m_id,
+            'reply_markup' => json_encode(array('inline_keyboard' => $kb))
+        );
+        $this->request('editMessageReplyMarkup', $data); 
+    }
+    
+    public function answerCallbackQuery($cb_id, $message) {
+        $data = array(
+            'callback_query_id'      => $cb_id,
+            'text'     => $message
+        );
+        $this->request('answerCallbackQuery', $data);
+    } 
+
+    public function sendChatAction($id,$action='typing') {
+        $data = array(
+            'chat_id' => $id,
+            'action'     => $action
+        );
+        $this->request('sendChatAction', $data);
+    }
+
+
     public  function request($method, $data = array()) {
-        $curl = curl_init(); 
+        $curl = curl_init();
           
         curl_setopt($curl, CURLOPT_URL, 'https://api.telegram.org/bot' . $this->token .  '/' . $method);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); 
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data); 
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
           
-        $out = json_decode(curl_exec($curl), true); 
+        $out = json_decode(curl_exec($curl), true);
           
-        curl_close($curl); 
-          
-        return $out; 
+        curl_close($curl);
+        return $out;
     }
 }
