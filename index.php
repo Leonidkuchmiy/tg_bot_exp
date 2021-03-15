3ced1f69317650a9d53bef3c9fb18dede1d136fa
Leonidkuchmiy <60760010+Leonidkuchmiy@users.noreply.github.com>
2021-03-15 23:19:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,41 @@
-$body = file_get_contents('php://input'); 
+
+<?php
+define('TGKEY', '1681935656:AAGG7bpYJFly7eMWpGnondSOrguLtT27mLo');
+include_once('tg.class.php');
+
+$body = file_get_contents('php://input');
 $arr = json_decode($body, true); 
- 
-include_once ('telegramgclass.php');   
 
-$tg = new tg('1681935656:AAGG7bpYJFly7eMWpGnondSOrguLtT27mLo');
+$tg = new tg(TGKEY);
 
-$chat_id = $arr['message']['chat']['id'];
-$userTgId = $arr['message']['from']['id'];
-$text = $arr['message']['text'];
-$coord1 = $arr['message']['location']['latitude'];
-$coord2 = $arr['message']['location']['longitude'];
+$tg_id = $arr['message']['chat']['id'];
+$rez_kb = array();
 
-$tg->send($chat_id, "Нас не догонят!");
+$message_text = $arr['message']['text'];
+$tg->sendChatAction($tg_id);
+$sms_rev='';
+	
+	switch($message_text){
+		case '/start':
+			$sms_rev = 'Здравствуйте, Вас приветсвует Простейший Бот Telegram!
+';
+		break;
+		case '/help':
+			$sms_rev = 'Я могу выполнить следующюю функцию:
+			/rev - переворачиваею строку наоборот.
+';	
+		break;	
+
+		case '/rev':
+			$sms_rev = strrev($message_text);	
+		break;	
+
+		default:
+			$sms_rev ='Команда не распознана';
+		break;	
+	}
+
+$tg->send($tg_id, $sms_rev, $rez_kb);
+exit('ok'); // говорим телеге, что все окей
+?>
+?>
